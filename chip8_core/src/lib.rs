ec2cedaf563b6e69333921f828c5fc76ae204534
macbulen <66035212+macbulen@users.noreply.github.com>
2024-11-16 14:47:25 -0500

    4 Implementing Emulation Methods
    Added the push and pop function to manage CPU's stack.
    Added a constant array called FONTSET to hold sprite data for characters 0-9 and A-F.
    Added reset function to reset emulator without having to create a new object.
    Added tick, fetch, and tick_timers function as a start to handling CPU processes

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -5,6 +5,26 @@ const NUM_REGS: usize = 16;
 const STACK_SIZE: usize = 16;
 const NUM_KEYS: usize = 16;
 const START_ADDR: u16 = 0x200;
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+0x20, 0x60, 0x20, 0x20, 0x70, // 1
+0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+];
 
 pub struct Emu {
     pc: u16,
@@ -21,7 +41,7 @@ pub struct Emu {
 
 impl Emu {
     pub fn new() -> Self {
-        Self {
+        let mut new_emu = Self {
             pc : START_ADDR,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
@@ -32,6 +52,64 @@ impl Emu {
             keys: [false; NUM_KEYS],
             delay_timer: 0,
             sound_timer: 0,
+        };
+
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+        
+        new_emu
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_registers = [0; NUM_REGS];
+        self.i_registers = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.delaytimer = 0;
+        self.soundtimer = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.sp -= 1;
+        self.stack[self.sp as usize];
+    }
+
+    pub fn tick(&mut self) {
+        //Fetch
+        let op = self.fetch();
+        //Decode
+        //Execute
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.delaytimer > 0 {
+            self.delaytimer -= 1;
+        }
+        
+        if self.soundtimer > 0 {
+            if self.soundtimer == 1 {
+                // BEEP
+            }
+            self.soundtimer -= 1;
         }
     }
+
 }
